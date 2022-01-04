0611748aa2fa9a563724c30dbd312bd2c7e4b176
Sitapascual <marialuisa.pascualr@um.es>
2022-01-04 18:02:44 +0100

    Añadido código

--- Test1GitProject/src/main/java/umu/tds/Test1GitProject/App.java
+++ Test1GitProject/src/main/java/umu/tds/Test1GitProject/App.java
@@ -1,5 +1,9 @@
 package umu.tds.Test1GitProject;
 
+import java.awt.List;
+import java.util.Arrays;
+import java.util.Collection;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,12 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Bye!" );
+    	
+    	List lista = (List) Arrays.asList("uno", "dos", "tres");
+    	
+    	int suma = (Integer) ((Collection<String>) lista).stream()
+    			.map(s -> s.length())
+    			.reduce(0,(s,ac) -> ac + s);
+    	System.out.println(suma);
     }
 }
